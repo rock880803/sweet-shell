ba82bb89d51e8e554b17cc780de332f764952e87
rock880803 <rock880803@gmail.com>
2022-06-03 18:08:01 +0800

    modified: style

--- js/all.js
+++ js/all.js
@@ -1,38 +1,54 @@
 
 const $isMobile = $(window).width();
 const $btn = $('.btn');
-const $btn1 = $('#Btn1');
-const $btn2 = $('#Btn2');
-const $btn3 = $('#Btn3');
-const $btn4 = $('#Btn4');
+const $Btn1 = $('#Btn1');
+const $Btn2 = $('#Btn2');
+const $Btn3 = $('#Btn3');
+const $Btn4 = $('#Btn4');
 const $SlideLs = $('#SlideLs');
 const $navItem = $('.nav-item > a');
+const $imgScroll = $('.linebot-body > .img-resp');
 
-$btn.click(function () {
-    $(this).addClass('js-nav-btn')
-    .parent().siblings().find('.btn').removeClass('js-nav-btn');
-})
+$btn.find('.btn-item').eq(0).addClass('js-nav-btn');
+
+$('#Next').click(function(){
+    $SlideLs.css({
+        top: '-100%'
+    });
+    $btn.find('.btn-item').eq(1).addClass('js-nav-btn')
+    .siblings().removeClass('js-nav-btn');
+});
 
 
-$btn1.click(function () {
+$Btn1.click(function () {
     $SlideLs.css({
         top: 0
     });
+    $btn.find('.btn-item').eq(0).addClass('js-nav-btn')
+    .siblings().removeClass('js-nav-btn');
     
 });
-$btn2.click(function () {
+$Btn2.click(function () {
     $SlideLs.css({
         top: '-100%'
     })
+    $btn.find('.btn-item').eq(1).addClass('js-nav-btn')
+    .siblings().removeClass('js-nav-btn');
 });
-$btn3.click(function () {
+$Btn3.click(function () {
     $SlideLs.css({
         top: '-200%'
     })
+    $btn.find('.btn-item').eq(2).addClass('js-nav-btn')
+    .siblings().removeClass('js-nav-btn');
 });
-$btn4.click(function () {
+$Btn4.click(function () {
     $SlideLs.css({
         top: '-300%'
     })
+    $btn.find('.btn-item').eq(3).addClass('js-nav-btn')
+    .siblings().removeClass('js-nav-btn');
 });
+
+$imgScroll.scrollTop()>=200 ? $imgScroll.addClass('js-img-scroll') : $imgScroll.removeClass('js-img-scroll')
 
