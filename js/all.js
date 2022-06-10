77621f9193c2cc0322d556e66712511b9f5d9dba
rock880803 <rock880803@gmail.com>
2022-06-10 17:01:09 +0800

    add: instruction

--- js/all.js
+++ js/all.js
@@ -2,30 +2,45 @@ const $isMobile = $(window).width();
 const $Slide = $('#Slide');
 const $SlideLs = $('#SlideLs');
 const $Nav = $('#Nav');
-const $NavItem = $('.nav-item > a');
+const $NavItem = $('.nav-hover > a');
 const $SlideItem = $('.slide-item');
-const $prevArrow = $('#prevArrow');
-const $homeArrow = $('#homeArrow');
-const $nextArrow = $('#nextArrow');
-const $gallery = $('[data-fancybox="gallery"]')
+const $gallery = $('[data-fancybox="gallery"]');
+const $img = $('img');
 let pageNum = 1;
+
+
+// language
+const $cn = $('.cn');
+const $en = $('.en');
+const $langText = $('#langText');
+$en.addClass('js-lang-btn')
+
+const $langBtn = {
+    cn: $('#cnBtn'),
+    en: $('#enBtn'),
+}
+$langBtn.cn.click(function () {
+    $en.addClass('js-lang-btn')
+    $cn.removeClass('js-lang-btn')
+    console.log('lang: chinese');
+})
+$langBtn.en.click(function () {
+    $cn.addClass('js-lang-btn')
+    $en.removeClass('js-lang-btn')
+    console.log('lang: english');
+})
+
 // Module
 const $slideDown = $('.slide-down .container');
 const $floatbtn = $('.float-btn');
 const $shadow = $('.shadow');
-const $linebotItem = $('#linebotItem');
 let num = -2;
 // 
 const counts = {
-    name1: "訊息傳送",
     data1: "11022",
-    name2: "加入好友",
     data2: "621",
-    name3: "活躍聊天室",
     data3: "985",
-    name4: "接收的訊息",
     data4: "8173",
-    name5: "傳送的訊息",
     data5: "4708",
 };
 
@@ -33,67 +48,80 @@ Object.keys(counts).forEach(function (key) {
     $(`[data-set=${key}]`).text(counts[key])
 });
 
+// Page
+
+const $instructionBtn = $('#instructionBtn');
+
+$instructionBtn.click(function () {
+    pageNum = 3;
+    pageSet()
+    $SlideItem.scrollTop(1000);
+
+    // $SlideItem.animate({
+    //     scrollTop: jQuery("#serviceItem").offset().top(100)
+    // }, 1000);
+});
+
+// jQuery('html, body').animate({
+//     scrollTop: jQuery("#serviceItem").offset().top
+// }, 1000);
+
 $NavItem.parent().eq(0).addClass('js-nav-btn');
 
 $NavItem.click(function () {
-    pageNum = $(this).parent().index() + 1;
+    pageNum = $(this).parent().index();
     pageSet()
 
-});
-
-
-$SlideItem.scroll(function () {
-    $(this).scrollTop() > 100 ? $Nav.addClass('js-nav') : $Nav.removeClass('js-nav')
-    console.log($(this).scrollTop());
 });
 
 setInterval(function () {
     num < -2 ? num++ : num = -3
-    $floatbtn.css({
-        'margin-top': num + '%',
-    })
+    $floatbtn.css({ 'margin-top': num + '%', })
 }, 800);
 
-$linebotItem.scroll(function () {
-    $(this).scrollTop() > 200 ? $slideDown.css({
-        'opacity': '0',
-    }) :
-        $slideDown.css({
-            'opacity': '1',
-        })
+$SlideItem.scroll(function () {
+    $(this).scrollTop() > 100 ? $Nav.addClass('js-nav') : $Nav.removeClass('js-nav')
+    $(this).scrollTop() > 100 ? $langText.addClass('js-lang-btn') : $langText.removeClass('js-lang-btn')
+    $(this).scrollTop() > 200 ? $slideDown.parent().css({ 'opacity': '0', }) : $slideDown.parent().css({ 'opacity': '1', })
+});
+//  instruction num
+const $instructionStep = $('.instruction-item h2');
+const $instructionLs = $('#instructionLs')
+const $insPrevArrow = $('#insPrevArrow');
+const $insNextArrow = $('#insNextArrow');
+
+
+
+let insNum = 1;
+$insNextArrow.click(function () {
+    insNum < 13 ? insNum++ : insNum = 13;
+    $instructionStep.text('step'+ insNum);
+    insPageSet()
+});
+$insPrevArrow.click(function () {
+    insNum > 1 ? insNum-- : insNum = 1;
+    $instructionStep.text('step'+ insNum);
+    insPageSet()
 });
 
 
-// arrow
-$nextArrow.click(function () {
-    pageNum < 5 ? pageNum++ : pageNum = 1;
-    pageSet()
-});
-$prevArrow.click(function () {
-    pageNum > 1 ? pageNum-- : pageNum = 5;
-    pageSet()
-});
-$homeArrow.click(function () {
-    pageNum = 1;
-    pageSet()
-});
-
-
-
+function insPageSet() {
+    $instructionLs.css({
+        top: - (insNum - 1) * 30 + 'vw',
+    })
+}
 function pageSet() {
     $NavItem.eq(pageNum - 1).parent().addClass('js-nav-btn').siblings().removeClass('js-nav-btn');
-    $NavItem.eq(pageNum - 1).find('span').html('<i class="fa-solid fa-hands-holding-circle fa-bounce">')
-    $NavItem.eq(pageNum - 1).parent().siblings().find('span').html('')
+    $NavItem.eq(pageNum - 1).find('span.icon').html('<i class="fa-solid fa-hands-holding-circle fa-bounce">')
+    $NavItem.eq(pageNum - 1).parent().siblings().find('span.icon').html('')
     $SlideLs.css({
         left: - (pageNum - 1) * 100 + '%',
     })
 }
 
-
-
-
 $gallery.fancybox({
     loop: true,
+    protect: true,
     keyboard: true,
     animationDuration: 500,
     transitionDuration: 800,
@@ -103,3 +131,4 @@ $gallery.fancybox({
         'close',
     ],
 });
+
